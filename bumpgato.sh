669974f59f481fc4365ce7aa651807f650afb505
Don Suhr <donald_suhr@intuit.com>
2023-06-13 11:12:07 -0700

    add bump script so safe place

---
+++ bumpgato.sh
@@ -0,0 +1,36 @@
+#!/bin/zsh
+
+export NVM_DIR=$HOME/.nvm;
+source $NVM_DIR/nvm.sh;
+
+VERSION_STRING='"version": '
+CURR_VERSION=$(awk -F \" '/"version": ".+"/ { print $4; exit; }' package.json)
+NEXT_VERSION=$(echo ${CURR_VERSION} | awk -F. -v OFS=. '{$NF += 1 ; print}')
+PACKAGE_NAME=$(awk -F \" '/"name": ".+"/ { print $4; exit; }' package.json)
+BUNDLE_PREFIX=$(echo ${PACKAGE_NAME} | sed -e "s/@//g" | sed -e "s/\//-/g")
+CWD=$(pwd)
+TARGET_DIR=$1
+
+# bump package verison so it doesnt cache the bundle
+sed -i '' "s/\($VERSION_STRING\).*/\1\"$NEXT_VERSION\",/" package.json
+
+echo "CWD: $CWD"
+echo "NEXT_VERSION: $NEXT_VERSION"
+echo "BUNDLE_PREFIX: $BUNDLE_PREFIX" 
+echo "TARGET_DIR: $TARGET_DIR" 
+
+nvm use
+yarn
+npm run build
+npm pack
+
+cd $TARGET_DIR
+echo "PWD: $PWD"
+nvm use
+yarn add $CWD/$BUNDLE_PREFIX-${NEXT_VERSION}.tgz
+# ./bump.sh
+
+cd $CWD
+nvm use
+
+echo '\a'
